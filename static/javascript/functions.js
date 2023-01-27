d4264096711ae7c1d721c1a25fcec1b9f0e8d945
Daniel Yakov <daniel577000@gmail.com>
2023-01-27 14:50:34 +0200

    complete UI

---
+++ static/javascript/functions.js
@@ -0,0 +1,85 @@
+// Show all employees
+function loadPage(){
+    $.get("http://localhost:3000/employees",function(employees){
+        for(let employee of employees){
+            $("#main").append(
+                `<div id=${employee._id} class="card mx-auto mb-2 col-md-4" style="width: 18rem;" data-editable> 
+                    <div class="card-body"> 
+                    <h4 class="card-title" id="name-${employee._id}">${employee.name}</h4> 
+                    <h6 class="card-title" id="position-${employee._id}">${employee.position ? employee.position : 'No position'}</h6> 
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">age: <span id="age-${employee._id}">${employee.age}</span></li>
+                        <li class="list-group-item">salary: <span id="salary-${employee._id}">${employee.salary}<span></li>
+                    </ul>
+                    <button id="edit-${employee._id}" class="btn btn-primary">Edit</button> 
+                    <button id="delete-${employee._id}" class="btn btn-danger">delete</button> 
+                    </div> 
+                </div>`
+            )  
+        }
+    });
+}
+
+// show form for editing
+function editForm(id) {
+    
+    // content of all files of specific card (with the id of the employee)
+    const name = $(`#name-${id}`).text()
+    const age = $(`#age-${id}`).text()
+    const salary = $(`#salary-${id}`).text()
+    const position = $(`#position-${id}`).text()
+
+    $(`#${id}`).replaceWith(
+        `<div class="card mx-auto mb-2 col-md-4" style="width: 18rem;" data-editable> 
+            <div class="card-body"> 
+                <form method="post">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" value=${name}>
+                    </div>
+                    <div class="mb-3">
+                        <label for="age" class="form-label">age</label>
+                        <input type="text" class="form-control" id="age" name="age" value=${age}>
+                    </div>
+                    <div class="mb-3">
+                        <label for="salary" class="form-label">salary</label>
+                        <input type="text" class="form-control" id="salary" name="salary" value=${salary}>
+                    </div>
+                    <div class="mb-3">
+                        <label for="position" class="form-label" >position</label>
+                        <input type="text" class="form-control" id="position" name="position" value=${position}>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div> 
+        </div>`
+    );
+}
+
+function addForm(){
+    $("#main").append(
+        `<div class="card mx-auto mb-2 col-md-4" style="width: 18rem;" data-editable> 
+            <div class="card-body"> 
+                <form method="post">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="age" class="form-label">age</label>
+                        <input type="text" class="form-control" id="age" name="age">
+                    </div>
+                    <div class="mb-3">
+                        <label for="salary" class="form-label">salary</label>
+                        <input type="text" class="form-control" id="salary" name="salary">
+                    </div>
+                    <div class="mb-3">
+                        <label for="position" class="form-label">position</label>
+                        <input type="text" class="form-control" id="position" name="position">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div> 
+        </div>`
+    )  
+}
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,26 +1,3 @@
-// Show all employees
-function loadPage(){
-    $.get("http://localhost:3000/employees",function(employees){
-        for(let employee of employees){
-            $("#main").append(
-                `<div id=${employee._id} class="card mx-auto mb-2 col-md-4" style="width: 18rem;" data-editable> 
-                    <div class="card-body"> 
-                    <h4 class="card-title">${employee.name}</h4> 
-                    <h6 class="card-title">${employee.position ? employee.position : 'No position'}</h6> 
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">age: ${employee.age}</li>
-                        <li class="list-group-item">salary: ${employee.salary}</li>
-                    </ul>
-                    <button id="edit-${employee._id}" class="btn btn-primary">Edit</button> 
-                    <button id="delete-${employee._id}" class="btn btn-danger">delete</button> 
-                    </div> 
-                </div>`
-            )  
-        }
-    });
-}
-
-
 $("#main").on("click", "button", function(){
     const method = this.id.split('-')[0]
     const id = this.id.split('-')[1]
@@ -28,31 +5,9 @@ $("#main").on("click", "button", function(){
     // edit button was pressed
     if (method === "edit"){
         // show form to enter changes
-        $(`#${id}`).replaceWith(
-            `<div class="card mx-auto mb-2 col-md-4" style="width: 18rem;" data-editable> 
-                <div class="card-body"> 
-                    <form action="http://localhost:3000/employees/${id}?_method=PUT" method="post">
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Name</label>
-                            <input type="text" class="form-control" id="name" name="name" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="age" class="form-label">age</label>
-                            <input type="text" class="form-control" id="age" name="age">
-                        </div>
-                        <div class="mb-3">
-                            <label for="salary" class="form-label">salary</label>
-                            <input type="text" class="form-control" id="salary" name="salary">
-                        </div>
-                        <div class="mb-3">
-                            <label for="position" class="form-label">position</label>
-                            <input type="text" class="form-control" id="position" name="position">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div> 
-            </div>`
-        );
+        editForm(id) //replace the card with form for editing
+        
+        // when submiting the edit form
         $("form").submit(function(e){
             e.preventDefault()
             const formData = new FormData(this);
@@ -80,6 +35,55 @@ $("#main").on("click", "button", function(){
             });
         })
     }
+    // delete record
+    else if (method === "delete"){
+        // send the put request and update record
+        $.ajax({
+            type: "delete",
+            url: `http://localhost:3000/employees/${id}`,
+            contentType: "application/json",
+            success: function(data){
+                console.log("success", data);
+                $("#main").empty().html(loadPage()); // show new cards with updated content
+            },
+            error: function(err){
+                console.log(err);
+            }
+        });
+    }
+})
+
+$("#add").on("click", function(){
+    
+    addForm()
+
+    // when submiting the add form
+    $("form").submit(function(e){
+        e.preventDefault()
+        const formData = new FormData(this);
+        // Create the json data
+        const jsonData = {
+            name: formData.get('name'),
+            age: formData.get('age'),
+            salary: formData.get('salary'),
+            position: formData.get('position')
+        }
+
+        // send the put request and add record
+        $.ajax({
+            type: "Post",
+            url: `http://localhost:3000/employees`,
+            data: JSON.stringify(jsonData),
+            contentType: "application/json",
+            success: function(data){
+                console.log("success", data);
+                $("#main").empty().html(loadPage()); // show new cards with updated content
+            },
+            error: function(err){
+                console.log(err);
+            }
+        });
+    })
 })
 
 // load cards when page is ready

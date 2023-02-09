9815b2e4426754cab61056375c5ce81387c4a3d0
Daniel Yakov <daniel577000@gmail.com>
2023-02-09 22:31:28 +0200

    fix connection.js

--- app-server/models/connection.js
+++ app-server/models/connection.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 
 module.exports.connectMongo = async function (dbUri){
+    let tries = 0
     while (true){
         try {
             await mongoose.connect(dbUri)
             console.log("monogDB connected");
             break
         } catch {
+            if (tries > 1) break
             console.log('Error connecting to MongoDB. Retrying...');
+            tries++
         }
     }
 } 
